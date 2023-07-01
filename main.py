283cb5d928e43e7157c2ea24121e3cd85cf343cc
PhillipPhan1024 <colPhilPhan@gmail.com>
2023-06-30 21:01:52 -0500

    Added file selection

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tabula import read_pdf
 import sys
 from test_selection import coords as cds
 from test_selection import QApplication, PdfViewer
+import tkinter as tk
+from tkinter import filedialog
 
 #TODO:
 # Create a GUI system to select page
@@ -11,6 +13,8 @@ from test_selection import QApplication, PdfViewer
 #    - Have a feature saying need to select over multiple pages, then combine after selection
 # CSS
 # Customizable table
+
+file_path = None
 
 def generate_checklist_table(dataframe):
     # Generate the HTML table
@@ -104,15 +108,22 @@ def generate_html_file(dataframe, filename, css_file="T:\VsCode\SylLink\SylLink\
 def main():
     # Read only page 3 of the file
     print(coords)
-    quizzes = read_pdf('T:\VsCode\SylLink\SylLink\Test_Syllabus.pdf', pages=[3], multiple_tables=False, lattice=True, stream=True, area=coords, encoding='latin-1')
+    quizzes = read_pdf(file_path, pages=[3], multiple_tables=False, lattice=True, stream=True, area=coords, encoding='latin-1')
     df = quizzes[0]
     generate_html_file(df, "checklist.html")
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    pdf_path = 'T:\VsCode\SylLink\SylLink\Test_Syllabus.pdf'
-    viewer = PdfViewer(pdf_path)
+    
+    root = tk.Tk()
+    root.withdraw()
+    
+    # open the file dialog box
+    file_path = filedialog.askopenfilename()
+    
+    # pdf_path = 'T:\VsCode\SylLink\SylLink\Test_Syllabus.pdf'
+    viewer = PdfViewer(file_path)
     coords = cds
     app.aboutToQuit.connect(main)
     sys.exit(app.exec_())
