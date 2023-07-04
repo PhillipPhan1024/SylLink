3aa02cacecdc2f7e85309b5d36dd48dfc35c51af
PhillipPhan1024 <colPhilPhan@gmail.com>
2023-07-04 03:16:01 -0500

    Simple notion database code

---
+++ notion.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime, timezone
+
+NOTION_TOKEN = ""
+DATABASE_ID = ""
+
+headers = {
+    "Authorization": f"Bearer {NOTION_TOKEN}",
+    "Notion-Version": "2022-06-28",
+    "Content-Type": "application/json"
+}
+
+def get_pages():
+    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
+    payload = {"page_size": 100}
+    response = requests.post(url, json=payload, headers=headers)
+    
+    data = response.json()
+    
+    results = data["results"]
+    return results
+
+pages = get_pages()
+for page in pages:
+    page_id = page["id"]
+    props = page["properties"]
+    url = props["URL"]["title"][0]["text"]["content"]
+    title = props["Title"]["rich_text"][0]["text"]["content"]
+    published = props["Published"]["date"]["start"]
+    published = datetime.fromisoformat(published)
+    print(url, title, published)
