d08f37c014911e859bce1c1b704af9f87e10bec5
PhillipPhan1024 <colPhilPhan@gmail.com>
2023-07-05 22:04:04 -0500

    CSV to notion w/o auto creating columns

--- main.py
+++ main.py
@@ -11,106 +11,25 @@ from tkinter import filedialog
 #    - Ask if need for multiple pages
 # Figure out how to handle tables that leak through multiple pages
 #    - Have a feature saying need to select over multiple pages, then combine after selection
-# CSS
-# Customizable table
 
 file_path = None
 
-def generate_checklist_table(dataframe):
-    # Generate the HTML table
-    checklist_table = ""
-    for index, row in dataframe.iterrows():
-        checklist_table += "<tr>"
-        checklist_table += f"<td><input type='checkbox' onclick='toggleRow(this, {index})'></td>"
-        for value in row.values[0:]:
-            checklist_table += f"<td>{value}</td>"
-        checklist_table += "</tr>"
+import csv
 
-    return checklist_table
-
-def read_custom_css(css_file):
-    with open(css_file, "r") as file:
-        css_content = file.read()
-    return css_content
-
-def generate_html_file(dataframe, filename, css_file="checklist.css"):
-    checklist_table = generate_checklist_table(dataframe)
-    headers = "\n".join([f"<th>{header}</th>" for header in dataframe.columns])
-
-    html_content = f"""
-    <html>
-    <head>
-        <title>Checklist Table</title>
-        <style>
-            table {{
-                border-collapse: collapse;
-                width: 100%;
-            }}
-            th, td {{
-                padding: 8px;
-                text-align: left;
-            }}
-            th {{
-                background-color: #f2f2f2;
-                color: #0080ff; /* Set the column title text color to #0080ff */
-            }}
-            .completed {{
-                background-color: green;
-            }}
-            {read_custom_css(css_file) if css_file else ''}
-        </style>
-        <script>
-            function toggleRow(checkbox, row) {{
-                var table = checkbox.closest('table');
-                var rows = table.getElementsByTagName('tr');
-                if (checkbox.checked) {{
-                    rows[row + 1].classList.add('completed');
-                }} else {{
-                    rows[row + 1].classList.remove('completed');
-                }}
-            }}
-            
-            function toggleMode() {{
-                var body = document.getElementsByTagName('body')[0];
-                body.classList.toggle('dark-mode');
-            }}
-        </script>
-        <style>
-            .dark-mode {{
-                background-color: #222;
-                color: #fff;
-            }}
-        </style>
-    </head>
-    <body>
-        <h1>Checklist Table</h1>
-        <button onclick="toggleMode()">Toggle Mode</button>
-        <table>
-            <thead>
-                <tr>
-                    <th>Complete</th>
-                    {headers}
-                </tr>
-            </thead>
-            <tbody>
-                {checklist_table}
-            </tbody>
-        </table>
-    </body>
-    </html>
-    """
-
-    with open(filename, "w", encoding="utf-8") as file:
-        file.write(html_content)
-
-
+def generate_csv_file(dataframe, filename):
+    with open(filename, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(dataframe.columns)  # Write the column headers
+        for row in dataframe.values:
+            cleaned_row = [str(cell).encode('utf-8', 'replace').decode('utf-8') for cell in row]
+            writer.writerow(cleaned_row)  # Write the data rows
 
 def main():
     # Read only page 3 of the file
     print(coords)
     quizzes = read_pdf(file_path, pages=[3], multiple_tables=False, lattice=True, stream=True, area=coords, encoding='latin-1')
     df = quizzes[0]
-    generate_html_file(df, "checklist.html")
+    generate_csv_file(df, "checklist.csv")
 
 
 if __name__ == "__main__":
--- notion.py
+++ notion.py
@@ -1,8 +1,8 @@
+import csv
 import requests
 from datetime import datetime, timezone
 
-NOTION_TOKEN = "secret_gO9CLZGrVxtCzO1Y5KyoKwix1xLbPNlrNxlLONz1KZu"
-DATABASE_ID = "c92fd11b1d6e43f78404898045e18d74"
+# Set your Notion token and database ID
 
 headers = {
     "Authorization": f"Bearer {NOTION_TOKEN}",
@@ -10,15 +10,15 @@ headers = {
     "Content-Type": "application/json"
 }
 
+# Function to create a new page in Notion
 def create_page(data: dict):
     create_url = "https://api.notion.com/v1/pages"
-
     payload = {"parent": {"database_id": DATABASE_ID}, "properties": data}
-
     res = requests.post(create_url, headers=headers, json=payload)
-    # print(res.status_code)
-    return res
+    return res.json()
 
+
+# Function to update an existing page in Notion
 def update_page(page_id: str, data: dict):
     url = f"https://api.notion.com/v1/pages/{page_id}"
 
@@ -27,70 +27,37 @@ def update_page(page_id: str, data: dict):
     res = requests.patch(url, json=payload, headers=headers)
     return res
 
-def delete_page(page_id: str):
-    url = f"https://api.notion.com/v1/pages/{page_id}"
+# Example usage: read CSV file and create pages
+csv_file = "checklist.csv"  # Path to your CSV file
 
-    payload = {"archived": True}
+with open(csv_file, "r") as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        quiz = row["Quiz"]
+        topics_covered = row["Topics Covered"]
+        textbook_sections = row["Textbook Sections"]
+        date_closed = row["Date Closed"]
 
-    res = requests.patch(url, json=payload, headers=headers)
-    return res
+        # Create the data dictionary based on the row values
+        data = {
+            "Quiz": {"title": [{"text": {"content": quiz}}]},
+            "Topics Covered": {"rich_text": [{"text": {"content": topics_covered}}]},
+            "Textbook Sections": {"rich_text": [{"text": {"content": textbook_sections}}]},
+            "Date Closed": {"rich_text": [{"text": {"content": date_closed}}]}
+        }
 
-def get_pages(num_pages=None):
-    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+        # Create a new page in Notion with the data
+        response = create_page(data)
+        created_page = response
+        created_page_id = created_page["id"]
+        print(f"Created page with ID: {created_page_id}")
 
-    get_all = num_pages is None
-    page_size = 100 if get_all else num_pages
+        # You can also update the page here if needed
+        new_date = datetime.now().astimezone(timezone.utc).isoformat()
+        update_data = {"Date Closed": {"rich_text": [{"text": {"content": new_date}}]}}
+        response = update_page(created_page_id, update_data)
+        updated_page = response.json()
+        print(f"Updated page with ID: {created_page_id}")
 
-    payload = {"page_size": page_size}
-    response = requests.post(url, json=payload, headers=headers)
-    
-    data = response.json()
-    
-    results = data["results"]
-    
-    # Comment this out to dump all data to a file
-    # import json
-    # with open('db.json', 'w', encoding='utf8') as f:
-    #    json.dump(data, f, ensure_ascii=False, indent=4)
-    
-    # USE IF MORE THAN 100 PAGES
-    
-    while data["has_more"] and get_all:
-        payload = {"page_size" : page_size, "start_cursor": data["next_cursor"]}
-        url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
-        response = requests.post(url, json=payload, headers=headers)
-        data = response.json()
-        results.extend(data["results"])
-    
-    return results
+print("Pages creation completed.")
 
-pages = get_pages()
-for page in pages:
-    page_id = page["id"]
-    props = page["properties"]
-    url = props["URL"]["title"][0]["text"]["content"]
-    title = props["Title"]["rich_text"][0]["text"]["content"]
-    published = props["Published"]["date"]["start"]
-    published = datetime.fromisoformat(published)
-    print(url, title, published)
-    
-
-# title = "Test Title"
-# description = "Test Description"
-# published_date = datetime.now().astimezone(timezone.utc).isoformat()
-# data = {
-#     "URL": {"title": [{"text": {"content": description}}]},
-#     "Title": {"rich_text": [{"text": {"content": title}}]},
-#     "Published": {"date": {"start": published_date, "end": None}}
-# }
-
-# create_page(data)
-
-page_id = "the page id"
-
-new_date = datetime(2023, 1, 15).astimezone(timezone.utc).isoformat()
-update_data = {"Published": {"date": {"start": new_date, "end": None}}}
-
-update_page(page_id, update_data)
-
-# delete_page(page_id)
